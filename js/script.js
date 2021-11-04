7d766c000963e2928ca166a91674cdd24fadaf51
AlbertoCiuf <alberton.ciufici@gmail.com>
2021-11-04 15:58:53 +0100

    definizione oggetti team members statici

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const members = [
+  {
+   name: 'Angela Caroll',
+   role: 'Chief Editor',
+   photo: 'img/angela-caroll-chief-editor.jpg'
+  },
+  {
+   name: 'Angela Lopez',
+   role: 'Social Media Manager',
+   photo: 'img/angela-lopez-social-media-manager.jpg'
+  },
+  {
+   name: 'Barbara Ramos',
+   role: 'Graphic Designer',
+   photo: 'img/angela-lopez-social-media-manager.jpg'
+  },
+  {
+   name: 'Scott Estrada',
+   role: 'Developer',
+   photo: 'img/scott-estrada-developer.jpg'
+  },
+  {
+   name: 'Walter Gordon',
+   role: 'Office Manager',
+   photo: 'img/walter-gordon-office-manager.jpg'
+  }
+];
+
+for (let index in members) {
+  const singleMember = members[index];
+
+  let memberName = singleMember.name;
+  let memberRole = singleMember.role;
+  let memberPhoto = singleMember.photo;
+
+
+}
