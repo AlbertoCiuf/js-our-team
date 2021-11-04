bc70636fc0c00d1a43fea1320d1774a5f7f9c363
AlbertoCiuf <alberton.ciufici@gmail.com>
2021-11-04 17:14:58 +0100

    personalizzazione con membri di the office

--- js/script.js
+++ js/script.js
@@ -3,29 +3,29 @@
 //array contenente gli obj per i vari membri
 const members = [
   {
-   name: 'Angela Caroll',
-   role: 'Chief Editor',
-   photo: 'angela-caroll-chief-editor.jpg'
+   name: 'Dwight Schrute',
+   role: 'Assistant to the Regional Manager',
+   photo: 'Dwight_Schrute.jpeg'
   },
   {
-   name: 'Angela Lopez',
-   role: 'Social Media Manager',
-   photo: 'angela-lopez-social-media-manager.jpg'
+   name: 'Jim Halpert',
+   role: 'Salesman',
+   photo: 'Jim-halpert.jpeg'
   },
   {
-   name: 'Barbara Ramos',
-   role: 'Graphic Designer',
-   photo: 'barbara-ramos-graphic-designer.jpg'
+   name: 'Kevin Malone',
+   role: 'Accountant',
+   photo: 'Kevin.jpeg'
   },
   {
-   name: 'Scott Estrada',
-   role: 'Developer',
-   photo: 'scott-estrada-developer.jpg'
+   name: 'Pam Beesley',
+   role: 'Office Administrator',
+   photo: 'Pam_Beesley.jpeg'
   },
   {
-   name: 'Walter Gordon',
-   role: 'Office Manager',
-   photo: 'walter-gordon-office-manager.jpg'
+   name: 'Stanley Hudson',
+   role: 'Salesman',
+   photo: 'stanley.jpeg'
   }
 ];
 
